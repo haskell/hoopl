854188aa0a17cc50cc81292947e0bc6c3069464a
Joao Dias <dias@cs.tufts.edu>
2010-03-19 14:42:43 -0400

    Some code for testing the Hoopl library, along with some tests.

---
+++ testing/EvalMonad.hs
@@ -0,0 +1,138 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, GADTs, EmptyDataDecls, PatternGuards, TypeFamilies, NamedFieldPuns , FlexibleInstances, MultiParamTypeClasses, TypeSynonymInstances #-}
+
+module EvalMonad (ErrorM, VarEnv, B, State,
+                  EvalM, runProg, inNewFrame, get_proc, get_block,
+                         get_var, set_var, get_heap, set_heap,
+                  Event (..), event) where
+
+import Control.Monad.Error
+import qualified Data.Map as M
+import Prelude hiding (succ)
+
+import CunningTransfers
+import IR
+
+type ErrorM        = Either String
+type InnerErrorM v = Either (State v, String)
+instance Error (State v, String) where
+  noMsg      = (undefined, "")
+  strMsg str = (undefined, str)
+
+data EvalM v a = EvalM (State v -> InnerErrorM v (State v, a))
+
+instance Monad (EvalM v) where
+  return x = EvalM (\s -> return (s, x))
+  EvalM f >>= k = EvalM $ \s -> do (s', x) <- f s
+                                   let EvalM f' = k x
+                                   f' s'
+instance MonadError String (EvalM v) where
+  throwError e = EvalM (\s -> throwError (s, e))
+  catchError (EvalM f) handler =
+    EvalM $ \s -> f s `catchError` handler'
+                where handler' (s', e) = let EvalM f' = handler e
+                                         in  f' s'
+
+-- Shorthands for frequently used types
+type VarEnv  v = M.Map Var  v
+type HeapEnv v = M.Map Addr v -- word addressed heap
+type Addr      = Integer
+type BEnv      = BlockEnv B
+type B         = Block Node C C
+type PEnv      = M.Map String Proc
+
+runProg :: [Proc] -> [v] -> EvalM v x -> ErrorM (State v, x)
+runProg procs vs (EvalM f) = 
+  case f init_state of
+    Left (_, e) -> throwError e
+    Right x     -> return x
+  where
+    init_state = State { frames = [], heap = M.empty, events = [],
+                         vsupply = vs, procs = procMap }
+    procMap = M.fromList $ zip (map name procs) procs
+  
+get_state :: EvalM v (State v)
+get_state = EvalM f
+  where f state = return (state, state)
+
+upd_state :: (State v -> State v) -> EvalM v ()
+upd_state upd = EvalM (\state -> return (upd state, ()))
+
+event :: Event v -> EvalM v ()
+event e = upd_state (\s -> s {events = e : events s})
+
+----------------------------------
+-- State of the machine
+data State v = State { frames  :: [(VarEnv v, BEnv)]
+                     , heap    :: HeapEnv v
+                     , procs   :: PEnv
+                     , vsupply :: [v]
+                     , events  :: [Event v]
+                     }
+data Event v = CallEvt String [v]
+             | RetEvt         [v]
+             | StoreEvt Addr v
+             | ReadEvt  Addr v
+
+get_var :: Var -> EvalM v v
+get_var var = get_state >>= k
+  where k (State {frames = (vars, _):_}) = mlookup "var" var vars
+        k _ = throwError "can't get vars from empty stack"
+
+set_var :: Var -> v -> EvalM v ()
+set_var var val = upd_state f
+  where f s@(State {frames = (vars, blocks):vs}) =
+            s { frames = (M.insert var val vars, blocks):vs }
+        f _ = error "can't set var with empty stack"
+
+-- Special treatment for the heap:
+-- If a heap location doesn't have a value, we give it one.
+get_heap :: Addr -> EvalM v v
+get_heap addr =
+  do State {heap, vsupply} <- get_state
+     (v, vs) <- case vsupply of v:vs -> return (v, vs)
+                                _    -> throwError "hlookup hit end of value supply"
+     upd_state (\s -> s {heap = M.insert addr v heap, vsupply = vs})
+     event $ ReadEvt addr v
+     return v
+
+set_heap :: Addr -> v -> EvalM v ()
+set_heap addr val =
+  do event $ StoreEvt addr val
+     upd_state $ \ s -> s { heap = M.insert addr val (heap s) }
+
+get_block :: BlockId -> EvalM v B
+get_block bid = get_state >>= k
+  where k (State {frames = (_, blocks):_}) = flookup "block" bid blocks
+        k _ = error "can't get blocks from empty stack"
+
+get_proc :: String -> EvalM v Proc
+get_proc name = get_state >>= mlookup "proc" name . procs
+
+newFrame :: VarEnv v -> [B] -> EvalM v ()
+newFrame vars blocks = upd_state $ \s -> s { frames = (vars, blockEnv) : frames s}
+  where blockEnv = mkBlockEnv (zip (map blockId blocks) blocks)
+
+popFrame :: EvalM v ()
+popFrame = upd_state f
+  where f s@(State {frames = _:fs}) = s { frames = fs }
+        f _ = error "popFrame: no frame to pop..." -- implementation error
+
+inNewFrame :: VarEnv v -> [B] -> EvalM v x -> EvalM v x
+inNewFrame vars blocks body =
+  do newFrame vars blocks
+     x <- body
+     popFrame
+     return x
+        
+generic_lookup :: String -> (k -> m -> Maybe v) -> k -> m -> EvalM v' v
+generic_lookup blame lookupf k m =
+  case lookupf k m of
+    Just v  -> return v
+    Nothing -> throwError ("unknown lookup for " ++ blame)
+
+mlookup :: Ord k => String -> k -> M.Map k v -> EvalM v' v
+mlookup blame = generic_lookup blame M.lookup
+
+flookup :: String -> BlockId -> BlockEnv a -> EvalM v a
+flookup blame = generic_lookup blame $ flip findBEnv
---
+++ testing/IR.hs
@@ -0,0 +1,124 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, GADTs, EmptyDataDecls, PatternGuards, TypeFamilies, NamedFieldPuns #-}
+module IR (Proc (..), Node (..), Exp (..), Value (..), BinOp(..), Var, showProc) where
+
+import Data.Maybe
+import Prelude hiding (succ)
+
+import CunningTransfers
+
+data Exp = Lit   Integer
+         | Var   Var
+         | Load  Exp
+         | Binop BinOp Exp Exp
+data BinOp = Add | Sub | Mul | Div | Eq | Ne | Lt | Gt | Lte | Gte
+type Var   = String
+data Value = B Bool | I Integer
+
+data Proc = Proc { name :: String, args :: [Var], body :: Graph Node C C }
+
+data Node e x where
+  Label  :: BlockId ->                                  Node C O
+  Assign :: Var     -> Exp     ->                       Node O O
+  Store  :: Exp     -> Exp     ->                       Node O O
+  Branch :: BlockId ->                                  Node O C
+  Cond   :: Exp     -> BlockId -> BlockId ->            Node O C
+  Call   :: [Var]   -> String  -> [Exp]   -> BlockId -> Node O C -- String is bogus
+  Return :: [Exp]   ->                                  Node O C
+
+instance Edges Node where
+  blockId (Label bid) = bid
+  blockId _           = error "GADT unreachable?"
+  successors (Branch b)     = [b]
+  successors (Cond _ b1 b2) = [b1, b2]
+  successors (Call _ _ _ b) = [b]
+  successors (Return _)     = []
+  successors _              = error "GADT unreachable?"
+
+-- Prettyprinting
+
+showProc :: Proc -> String
+showProc proc = name proc ++ tuple (args proc) ++ graph
+  where
+    graph  = " {\n" ++ showG showBID (body proc) ++ "}\n"
+    -- Get all the block IDs and map them to distinct integers
+    showBID = show . fromJust . (findBEnv $ mkBlockEnv $ zip bids nats)
+    nats = [1..] :: [Integer]
+    bids = foldG getBID [] (body proc)
+    getBID :: forall e' x'. [BlockId] -> Node e' x' -> [BlockId]
+    getBID rst (Label bid)  = bid   : rst
+    getBID rst (Branch bid) = bid   : rst
+    getBID rst (Cond _ t f) = t : f : rst
+    getBID rst _            = rst
+
+foldG :: (forall e' x'. a -> Node e' x' -> a) -> a -> Graph Node e x -> a
+foldG _ z GNil = z
+foldG f z (GUnit b) = foldB f z b
+foldG f z (GMany {g_entry, g_blocks, g_exit}) =
+  foldB f (foldl (foldB f) (foldExit (foldB f) z g_exit) g_blocks) g_entry
+
+foldExit :: (a -> t C x -> a) -> a -> Exit t x -> a
+foldExit _ z NoTail   = z
+foldExit f z (Tail t) = f z t
+
+foldB :: (forall e' x'. a -> Node e' x' -> a) -> a -> Block Node e x -> a
+foldB f z (BUnit n)    = f z n
+foldB f z (BCat b1 b2) = foldB f (foldB f z b2) b1
+
+showG :: (BlockId -> String) -> Graph Node e x -> String
+showG _ GNil = ""
+showG b (GUnit block) = showB b block
+showG b (GMany {g_entry, g_blocks, g_exit}) =
+  showB b g_entry ++ concatMap (showB b) g_blocks ++ showExit (showB b) g_exit
+
+showExit :: (n C x -> String) -> Exit n x -> String
+showExit _ NoTail = ""
+showExit p (Tail n) = p n
+
+showB :: (BlockId -> String) -> Block Node e x -> String
+showB b (BUnit n) = showNode b n ++ "\n"
+showB b (BCat b1 b2) = showB b b1 ++ showB b b2
+
+
+showNode :: (BlockId -> String) -> Node e x -> String
+showNode b (Label bid)        = b bid ++ ":"
+showNode _ (Assign v e)       = ind $ v ++ " = " ++ show e
+showNode _ (Store addr e)     = ind $ "m[" ++ show addr ++ "] = " ++ show e
+showNode b (Branch bid)       = ind $ "goto " ++ b bid
+showNode b (Cond e t f)       =
+  ind $ "if " ++ show e ++ " then goto " ++ b t ++ " else goto " ++ b f
+showNode b (Call ress f cargs succ) =
+  ind $ tuple ress ++ " = " ++ f ++ tuple (map show cargs) ++ " goto " ++ b succ
+showNode _ (Return      rargs) = ind $ "ret " ++ tuple (map show rargs)
+
+ind :: String -> String
+ind x = "  " ++ x
+
+tuple :: [String] -> String
+tuple []     = "()"
+tuple [a]    = "(" ++ a ++ ")"
+tuple (a:as) = "(" ++ a ++ concat (map ((++) ", ") as) ++ ")"
+
+instance Show Exp where
+  show (Lit   i) = show i
+  show (Var   v) = v
+  show (Load  e) = "m[" ++ show e ++ "]"
+  show (Binop b e1 e2) = sub e1 ++ " " ++ show b ++ " " ++ sub e2
+    where sub e@(Binop _ _ _) = tuple [show e]
+          sub e = show e
+
+instance Show Value where
+  show (B b) = show b
+  show (I i) = show i
+
+instance Show BinOp where
+  show Add  = "+"
+  show Sub  = "-"
+  show Mul  = "*"
+  show Div  = "/"
+  show Eq   = "="
+  show Ne   = "/="
+  show Gt   = ">"
+  show Lt   = "<"
+  show Gte  = ">="
+  show Lte  = "<="
