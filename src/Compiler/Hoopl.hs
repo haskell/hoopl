167c6bd8cb587c77b7c6a0a7f32ff168284e9477
Norman Ramsey <nr@cs.tufts.edu>
2010-04-05 12:53:33 -0400

    defined module Compiler.Hoopl, which re-exports everything useful to the client, which is just about everything *except* the internal Compiler.Hoopl.GraphUtil (clients should use Compiler.Hoopl.MkGraph instead).
    Also changed version number to 3.7.0.0
    
      3 -- third major way of doing things
      7 -- Hoopl7.hs
      0 -- major revision number, should change when interface changes
      0 -- minor revision, should change when implementation changes
           in ways visible to clients

---
+++ src/Compiler/Hoopl.hs
@@ -0,0 +1,14 @@
+module Compiler.Hoopl
+  ( module Compiler.Hoopl.Dataflow
+  , module Compiler.Hoopl.Fuel
+  , module Compiler.Hoopl.Graph
+  , module Compiler.Hoopl.Label
+  , module Compiler.Hoopl.MkGraph
+  )
+where
+
+import Compiler.Hoopl.Dataflow
+import Compiler.Hoopl.Fuel
+import Compiler.Hoopl.Graph
+import Compiler.Hoopl.Label hiding (allLabels)
+import Compiler.Hoopl.MkGraph
