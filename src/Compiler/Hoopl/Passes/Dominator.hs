d8f887353625f581ac21c6cfb7f336da34bb6d2e
Norman Ramsey <nr@cs.tufts.edu>
2010-04-15 14:55:43 -0400

    added sample compiler pass (side branch)
    this one implements dominators

---
+++ src/Compiler/Hoopl/Passes/Dominator.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -Wall -fno-warn-name-shadowing #-}
+
+module Compiler.Hoopl.Passes.Dominator
+  ( Doms, domLattice, extendDom
+  , DominatorNode(..), DominatorTree(..), tree
+  , domFirst, domMiddle, domLast
+  )
+where
+
+import Data.Maybe
+
+import Compiler.Hoopl 
+
+type Doms = [Label] -- might hide this one day
+
+extendDom :: Label -> Doms -> Doms
+extendDom = (:)
+
+data DominatorNode = Entry | Labelled Label
+data DominatorTree = Dominates DominatorNode [DominatorTree]
+
+-- ^ This data structure is a *rose tree* in which each node may have
+--  arbitrarily many children.
+
+tree :: [(Label, Doms)] -> DominatorTree
+tree facts = Dominates Entry $ merge $ map reverse $ map (uncurry (:)) facts
+  where merge lists = mapTree $ children $ filter (not . null) lists
+        children = foldl addList noFacts
+        addList :: FactBase [[Label]] -> [Label] -> FactBase [[Label]]
+        addList map (x:xs) = extendFactBase map x (xs:existing)
+            where existing = fromMaybe [] $ lookupFact map x
+        addList _ [] = error "this can't happen"
+        mapTree :: FactBase [[Label]] -> [DominatorTree]
+        mapTree map = [Dominates (Labelled x) (merge lists) |
+                                                    (x, lists) <- factBaseList map]
+
+
+
+domLattice :: DataflowLattice Doms
+domLattice = DataflowLattice 
+        { fact_name = "dominators"
+        , fact_bot  = []
+        , fact_extend = extend
+        , fact_do_logging = True
+        }
+
+extend :: JoinFun Doms
+extend (OldFact l) (NewFact l') = (changeIf (length l /= length join), join)
+    where join = lcs l l'
+          lcs :: Doms -> Doms -> Doms -- longest common suffix
+          lcs l l' | length l > length l' = lcs (drop (length l - length l') l) l'
+                   | length l < length l' = lcs l' l
+                   | otherwise = dropUnlike l l' l
+          dropUnlike [] [] maybe_like = maybe_like
+          dropUnlike (x:xs) (y:ys) maybe_like =
+              dropUnlike xs ys (if x == y then maybe_like else xs)
+          dropUnlike _ _ _ = error "this can't happen"
+
+
+domFirst  :: Edges n => n C O -> FactBase f -> f
+domMiddle :: Edges n => n O O -> f -> f
+domLast   :: Edges n => n O C -> f -> FactBase f
+
+domFirst n f = fromJust $ lookupFact f (entryLabel n)
+domMiddle _ = id
+domLast n f = mkFactBase [(l, f) | l <- successors n]
+
