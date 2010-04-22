7e581bb0b65b999811aaf8283fab47df00e9204b
Norman Ramsey <nr@cs.tufts.edu>
2010-04-21 20:58:48 -0400

    expose Top; add utility functions for making transfer functions

--- src/Compiler/Hoopl/XUtil.hs
+++ src/Compiler/Hoopl/XUtil.hs
@@ -2,11 +2,15 @@
 
 module Compiler.Hoopl.XUtil
   ( WithBot(..), addBot, addBot'
---  , WithTop(..), addTop, addTop
+  , WithTop(..), addTop, addTop'
+  , firstXfer, distributeXfer, distributeFact
   )
 where
 
+import Data.Maybe
+
 import Compiler.Hoopl.Label
+import Compiler.Hoopl.Graph
 import Compiler.Hoopl.Dataflow
 
 -- | Adds a bottom element to a set to help form a lattice
@@ -68,3 +72,26 @@ instance Show a => Show (WithTop a) where
 instance Functor WithTop where
   fmap _ Top = Top
   fmap f (NonTop a) = NonTop (f a)
+
+
+----------------------------------------------------------------
+
+-- | A utility function so that a transfer function for a first
+-- node can be given just a fact; we handle the lookup.  This
+-- function is planned to be made obsolete by changes in the dataflow
+-- interface.
+
+firstXfer :: Edges n => (n C O -> f -> f) -> (n C O -> FactBase f -> f)
+firstXfer xfer n fb = xfer n $ fromJust $ lookupFact fb $ entryLabel n
+
+-- | This utility function handles a common case in which a transfer function
+-- produces a single fact out of a last node, which is then distributed
+-- over the outgoing edges.
+distributeXfer :: Edges n => (n O C -> f -> f) -> (n O C -> f -> FactBase f)
+distributeXfer xfer n f = mkFactBase [ (l, xfer n f) | l <- successors n ]
+
+-- | This utility function handles a common case in which a transfer function
+-- for a last node takes the incoming fact unchanged and simply distributes
+-- that fact over the outgoing edges.
+distributeFact :: Edges n => n O C -> f -> FactBase f
+distributeFact n f = mkFactBase [ (l, f) | l <- successors n ]
