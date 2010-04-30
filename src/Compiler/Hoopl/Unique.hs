42ae888850c39dae40f3db1ed81ec4d1eb154828
Norman Ramsey <nr@cs.tufts.edu>
2010-04-30 15:39:12 -0400

    Added some technology by which clients could create instances of HooplMonad.

--- src/Compiler/Hoopl/Unique.hs
+++ src/Compiler/Hoopl/Unique.hs
@@ -1,6 +1,8 @@
 module Compiler.Hoopl.Unique
   ( Unique, intOfUniq, uniqOfInt
   , HooplMonad(..)
+  , SimpleHooplMonad, runSimpleHooplMonad
+  , HooplMonadT, runHooplMonadT
   )
 
 where
@@ -20,3 +22,35 @@ instance Show Unique where
 
 class Monad m => HooplMonad m where
   freshUnique :: m Unique
+
+----------------------------------------------------------------
+
+newtype SimpleHooplMonad a = SHM { unSHM :: [Unique] -> (a, [Unique]) }
+
+instance Monad SimpleHooplMonad where
+  return a = SHM $ \us -> (a, us)
+  m >>= k  = SHM $ \us -> let (a, us') = unSHM m us in
+                              unSHM (k a) us'
+
+instance HooplMonad SimpleHooplMonad where
+  freshUnique = SHM $ \(u:us) -> (u, us)
+
+runSimpleHooplMonad :: SimpleHooplMonad a -> a
+runSimpleHooplMonad m = fst (unSHM m allUniques)
+
+----------------------------------------------------------------
+
+newtype HooplMonadT m a = HM { unHM :: [Unique] -> m (a, [Unique]) }
+
+instance Monad m => Monad (HooplMonadT m) where
+  return a = HM $ \us -> return (a, us)
+  m >>= k  = HM $ \us -> do { (a, us') <- unHM m us; unHM (k a) us' }
+
+instance Monad m => HooplMonad (HooplMonadT m) where
+  freshUnique = HM $ \(u:us) -> return (u, us)
+
+runHooplMonadT :: Monad m => HooplMonadT m a -> m a
+runHooplMonadT m = do { (a, _) <- unHM m allUniques; return a }
+
+allUniques :: [Unique]
+allUniques = map Unique [1..]
