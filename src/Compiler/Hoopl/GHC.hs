f6f20f137fe37f4c5ac423a12f5a5680880dea3a
Milan Straka <fox@ucw.cz>
2010-05-03 17:56:51 +0100

    Temporarily exporting getFuel and setFuel for GHC.
    It should become exported for all clients in the future.

--- src/Compiler/Hoopl/GHC.hs
+++ src/Compiler/Hoopl/GHC.hs
@@ -2,8 +2,10 @@
 module Compiler.Hoopl.GHC
   ( uniqueToInt
   , uniqueToLbl, lblToUnique
+  , getFuel, setFuel
   )
 where
 
 import Compiler.Hoopl.Label
 import Compiler.Hoopl.Unique
+import Compiler.Hoopl.Fuel
