89238e6a5b0f661b65c4563e466a8776ca061d6b
Milan Straka <fox@ucw.cz>
2010-05-05 19:53:18 +0100

    Exporing Body and Block representation to GHC.
    This is _shortterm_ only.
    
    When converting the GHC code to use Hoopl, various folds
    and maps over Graphs and Blocks are necessary. We are
    touching Body(..) and Block(..) strictly in one module only
    and after the conversion is done, reasonable set of these
    methods will go back to Hoopl.

--- src/Compiler/Hoopl/GHC.hs
+++ src/Compiler/Hoopl/GHC.hs
@@ -3,9 +3,11 @@ module Compiler.Hoopl.GHC
   ( uniqueToInt
   , uniqueToLbl, lblToUnique
   , getFuel, setFuel
+  , Body'(..), Block(..)
   )
 where
 
+import Compiler.Hoopl.Fuel
+import Compiler.Hoopl.Graph
 import Compiler.Hoopl.Label
 import Compiler.Hoopl.Unique
-import Compiler.Hoopl.Fuel
