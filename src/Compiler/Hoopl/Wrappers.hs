810d4a5f8f9f19e4377d7f75dcb170375b319adc
Norman Ramsey <nr@cs.tufts.edu>
2010-07-20 14:18:48 -0400

    exposed FwdRew and BwdRew constructors.  Perhaps we've lost our minds?

--- src/Compiler/Hoopl/Wrappers.hs
+++ src/Compiler/Hoopl/Wrappers.hs
@@ -1,5 +1,5 @@
-module Compiler.Hoopl.Wrappers {-# DEPRECATED "Use only if you known what you are doing and can preserve the 'respects fuel' invariant" #-}
-  ( wrapFR, wrapFR2, wrapBR, wrapBR2
+module Compiler.Hoopl.Wrappers {-# DEPRECATED "Use only if you know what you are doing and can preserve the 'respects fuel' invariant" #-}
+  ( wrapFR, wrapFR2, wrapBR, wrapBR2, FwdRew(..), BwdRew(..)
   )
 where
 
