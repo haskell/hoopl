ab18ac6472ee6a32e86b98de42069457826a7a54
Michal Terepeta <michal.terepeta@gmail.com>
2016-01-17 17:10:50 +0100

    Remove Checkpoint module
    Apparently the CheckpointingMonad wasn't actually used anywhere
    in the code (there were only a few instances). In particular the
    fixpoint algorithm did not use it at all.
    
    Removing it seems like a reasonable idea (at least for now), since:
    - it's not used,
    - having it is confusing (both for developers and users),
    - re-adding it's functionality might have performance implications
      and we don't yet have any reasonable benchmarks.
    So I'd prefer to first do some cleanup of the library & work on it's
    performance (e.g., adding some benchmarks) and only once everything
    starts to look good, consider adding new functionality.

--- src/Compiler/Hoopl/Unique.hs
+++ src/Compiler/Hoopl/Unique.hs
@@ -18,7 +18,6 @@ module Compiler.Hoopl.Unique
 
 where
 
-import Compiler.Hoopl.Checkpoint
 import Compiler.Hoopl.Collections
 
 import qualified Data.IntMap as M
@@ -128,11 +127,6 @@ instance UniqueMonad SimpleUniqueMonad where
   freshUnique = SUM $ f
     where f (u:us) = (u, us)
           f _ = error "Unique.freshUnique(SimpleUniqueMonad): empty list"
-
-instance CheckpointMonad SimpleUniqueMonad where
-  type Checkpoint SimpleUniqueMonad = [Unique]
-  checkpoint = SUM $ \us -> (us, us)
-  restart us = SUM $ \_  -> ((), us)
 
 runSimpleUniqueMonad :: SimpleUniqueMonad a -> a
 runSimpleUniqueMonad m = fst (unSUM m allUniques)
